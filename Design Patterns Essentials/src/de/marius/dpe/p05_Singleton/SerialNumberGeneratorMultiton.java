d2413fcd2c42a56433f60b5918d7a46b3c7fe20b
marius.muntean@live.com <github2me>
2014-12-03 15:15:17 +0100

    Added a comment to the "Multiton" implementation

--- Design Patterns Essentials/src/de/marius/dpe/p05_Singleton/SerialNumberGeneratorMultiton.java
+++ Design Patterns Essentials/src/de/marius/dpe/p05_Singleton/SerialNumberGeneratorMultiton.java
@@ -1,5 +1,11 @@
 package de.marius.dpe.p05_Singleton;
 
+/**
+ * A singleton that can generate two distinct sequences of serial numbers.
+ * 
+ * @author Marius
+ *
+ */
 public enum SerialNumberGeneratorMultiton {
 
 	ENGINE, VEHICLE;
